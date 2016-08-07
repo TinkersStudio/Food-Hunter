ce748374b3b7a454442cc341e67271087bcff6bc
Jun Trinh <dungtrinh1993@gmail.com>
2016-08-07 14:55:39 -0700

    update all the files. Implement RecycleView

--- app/src/main/java/edu/hackathon/foodhunter/EventView.java
+++ app/src/main/java/edu/hackathon/foodhunter/EventView.java
@@ -1,7 +1,71 @@
 package edu.hackathon.foodhunter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 /**
  * Created by kid on 8/6/16.
  */
-public class EventView {
+
+/** This is used with linear layout. Current version is recycle layout*/
+public class EventView extends LinearLayout {
+
+    /**The event*/
+    private Event event;
+
+    /**
+     * Basic mapping of the block
+     * 1    2
+     * 3    4
+     * 5
+     */
+    /**1. Type of food for the even*/
+    private TextView e_foodText;
+    /**2. Day of the event*/
+    private TextView e_dayText;
+    /**3. Location of the event*/
+    private TextView e_locationText;
+    /**4. Time of the event*/
+    private TextView e_timeText;
+    /**5. Event type*/
+    private TextView e_eventText ;
+
+    /**
+     * Constructor for the recycle view
+     * @param context app
+     * @param event the event that is created
+     */
+    public EventView(Context context, Event event) {
+        super(context);
+
+        /* rootView */
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.event_recyle_view, this, true);
+
+        /*Initialize the component*/
+        this.e_foodText = (TextView) findViewById(R.id.food_text_view);
+        this.e_dayText = (TextView) findViewById(R.id.day_text_view);
+        this.e_locationText = (TextView) findViewById(R.id.location_text_view);
+        this.e_timeText = (TextView) findViewById(R.id.time_text_view);
+        this.e_eventText = (TextView) findViewById(R.id.event_text_view);
+
+        this.setEvent(event);
+        requestLayout();
+    }
+
+    /**
+     * Mutator to change the event diplay
+     * @param event
+     */
+    public void  setEvent(Event event) {
+        this.event = event;
+
+        this.e_foodText.setText(event.getFood());
+        this.e_dayText.setText(event.getDate());
+        this.e_locationText.setText(event.getLocation());
+        this.e_timeText.setText(event.getTime());
+        this.e_eventText.setText(event.getEvent());
+    }
 }
--- app/src/main/java/edu/hackathon/foodhunter/EventViewHolder.java
+++ app/src/main/java/edu/hackathon/foodhunter/EventViewHolder.java
@@ -1,7 +1,50 @@
 package edu.hackathon.foodhunter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by kid on 8/6/16.
  */
-public class EventViewHolder {
+public class EventViewHolder extends RecyclerView.ViewHolder {
+
+    private Event event;
+    /**1. Type of food for the even*/
+    private TextView e_foodText;
+    /**2. Day of the event*/
+    private TextView e_dayText;
+    /**3. Location of the event*/
+    private TextView e_locationText;
+    /**4. Time of the event*/
+    private TextView e_timeText;
+    /**5. Event type*/
+    private TextView e_eventText ;
+
+    public EventViewHolder(View itemView) {
+        /**call to super*/
+        super(itemView);
+
+        /*Initialize the component*/
+        this.e_foodText = (TextView) itemView.findViewById(R.id.food_text_view);
+        this.e_dayText = (TextView) itemView.findViewById(R.id.day_text_view);
+        this.e_locationText = (TextView) itemView.findViewById(R.id.location_text_view);
+        this.e_timeText = (TextView) itemView.findViewById(R.id.time_text_view);
+        this.e_eventText = (TextView) itemView.findViewById(R.id.event_text_view);
+    }
+
+    /**
+     * Mutator to change the event diplay
+     * @param event
+     */
+    public void  setEvent(Event event) {
+        this.event = event;
+
+        this.e_foodText.setText(event.getFood());
+        this.e_dayText.setText(event.getDate());
+        this.e_locationText.setText(event.getLocation());
+        this.e_timeText.setText(event.getTime());
+        this.e_eventText.setText(event.getEvent());
+
+    }
 }

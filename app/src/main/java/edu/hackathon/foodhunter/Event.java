9a7c9dbb1b9b157a813b0eb68c8a3ea74032a4c0
Jun Trinh <dungtrinh1993@gmail.com>
2016-08-07 16:50:44 -0700

    Implement the basic function of the app

--- app/src/main/java/edu/hackathon/foodhunter/Event.java
+++ app/src/main/java/edu/hackathon/foodhunter/Event.java
@@ -1,9 +1,13 @@
 package edu.hackathon.foodhunter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by kid on 8/6/16.
  */
-public class Event
+public class Event implements Comparable<Event>
 {
     /**Type of food*/
     private String food;
@@ -15,6 +19,12 @@ public class Event
     /**Event*/
     private String event;
 
+    /**This is format and use internal to sort event based on date of the event*/
+    private Date dateFormat;
+
+    /**To order the list based on date created*/
+    private Date dateCreated;
+
     /**location of the event*/
     private String location;
     public Event( String event, String location, String food, String date, String time) {
@@ -23,8 +33,25 @@ public class Event
         this.date = date;
         this.time = time;
         this.event = event;
+        if (!setDateFormat(this.date)) {
+            this.food = "ERROR";
+            this.location = "404";
+        }
+
     }
 
+    protected boolean setDateFormat(String date) {
+        SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            this.dateFormat = dateformat.parse(date);
+            return true;
+        }
+        catch (ParseException e) {
+            //the origin of time to push it to the bottom
+            this.dateFormat.setTime(0);
+            return false;
+        }
+    }
     public void setFood (String food) {
         this.food = food;
     }
@@ -72,5 +99,10 @@ public class Event
         return (event1.getDate().equals(this.date) && event1.getEvent().equals(this.event)
                 && event1.getFood().equals(this.food) && event1.getTime().equals(this.time)&&
                 event1.getLocation().equals(this.location));
+    }
+
+    @Override
+    public int compareTo(Event event) {
+        return 0;
     }
 }
--- app/src/main/java/edu/hackathon/foodhunter/EventList.java
+++ app/src/main/java/edu/hackathon/foodhunter/EventList.java
@@ -1,24 +1,80 @@
 package edu.hackathon.foodhunter;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * Main activity class
  */
 public class EventList extends AppCompatActivity {
 
+
+    /**List of event*/
+    protected ArrayList<Event> eventList;
+
+    /*Sorted list*/
+    protected ArrayList<Event> sortedList;
+
+    /**ViewGroup for maintaining the list of view*/
+    protected RecyclerView eventLayout;
+
+    /**Adapter to bind AdapterView to list of events*/
+    protected EventListAdapter eventAdapter;
+
+    /**Menu used the app*/
+    protected Menu mainMenu;
+
+    /*Debugging TAG. Use in log*/
+    private static final String TAG = Activity.class.getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /*The origin list*/
+        this.eventList = new ArrayList<>();
+        this.sortedList = new ArrayList<>();
+
+        /*Setup the adapter view*/
+        eventAdapter = new EventListAdapter(this.eventList);
+        eventLayout.setAdapter(eventAdapter);
+
+        //TODO: Implement some dummy test in resource
+        ArrayList<Event> downloadedEvent = this.getEventList();
+        for (Event e:downloadedEvent) {
+            //adding the event and notify the changes
+            this.addEvent(e);
+        }
+
+        //TODO: Implement call back so when the user pull down the list is reloaded
     }
 
+    //TODO: Implement onSaveInstanceState and onRestoreInstanceState
+
     /**
-     * Initilize alll of the layout
+     * Initialize all of the layout
      */
-    protected void initLayout() {
+    protected void initLayout()
+    {
         setContentView(R.layout.main_activity);
+        this.eventLayout = (RecyclerView) findViewById(R.id.eventRecyclerViewGroup);
+        /**Layout manager*/
+        //because rotate is disable => always true
+        this.eventLayout.setLayoutManager(new LinearLayoutManager
+                (this,LinearLayoutManager.VERTICAL, false));
     }
 
     /**
@@ -26,5 +82,96 @@ public class EventList extends AppCompatActivity {
      */
     protected void initListener() {
 
+    }
+
+    /**
+     * Create the top menu level
+     * Init all of the component of the top menu
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        this.mainMenu = menu;
+        return true;
+    }
+
+    /**
+     * Option select (top menu)
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        //have to use the selection above
+        switch (item.getItemId()) {
+            case R.id.menu_filter:
+                sort();
+                //point the list over
+                eventList = sortedList;
+                break;
+            case R.id.top_menu_add:
+                updateEvent();
+                break;
+            case R.id.submenu_share:
+                //TODO: Implement share
+                Toast.makeText(this, "The function is not yet completed",
+                        Toast.LENGTH_LONG).show();
+                break;
+            default:
+                super.onOptionsItemSelected(item);
+                break;
+        }
+
+        //data will be changed so need to notify adapter
+        eventAdapter.notifyDataSetChanged();
+        return true;
+    }
+    /**
+     * This will download the list of the event from the server
+     * @return
+     */
+    protected ArrayList<Event> getEventList() {
+        //Retrieve the list of event from Firebase server
+        return new ArrayList<Event>();
+    }
+
+    /**
+     * Add the event to the list and notify the adapter
+     * @param event
+     */
+    protected void addEvent(Event event) {
+        this.sortedList.add(event);
+        this.eventList.add(event);
+        this.eventAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Sort the list of the event to display to the screen
+     */
+    protected void sort() {
+        //TODO: Sort the list of events
+        //SortedSet set = Collections.synchronizedSortedSet((SortedSet) eventList);
+        Collections.sort(sortedList);
+        eventAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Update the event to the server
+     */
+    protected void updateEvent () {
+        //Open the popup fragment on top of the old one
+    }
+
+    /**
+     * Load the display of the main screen
+     */
+    protected void loadMainScreen () {
+        //TODO: Need to implement
+        sortedList = getEventList();
     }
 }

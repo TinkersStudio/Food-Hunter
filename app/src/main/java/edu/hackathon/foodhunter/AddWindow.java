88db0df44996f41feb2270475d818bc11d1c22b5
Jun Trinh <dungtrinh1993@gmail.com>
2016-09-13 20:58:06 -0700

    - Update the TextView to EditText - Error: Cancelling event due to no window focus: MotionEvent

--- app/src/main/java/edu/hackathon/foodhunter/AddWindow.java
+++ app/src/main/java/edu/hackathon/foodhunter/AddWindow.java
@@ -1,10 +1,13 @@
 package edu.hackathon.foodhunter;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,18 +27,30 @@ import java.util.Objects;
  */
 public class AddWindow extends Activity{
 
+    /**Button group*/
     protected Button huntButton;
     protected Button cancelButton;
 
-    protected TextView foodText;
-    protected TextView locationText;
-    protected TextView dateText;
-    protected TextView timeText;
-    protected TextView eventText;
+    /**Edit text box*/
+    protected EditText foodText;
+    protected EditText locationText;
+    protected EditText dateText;
+    protected EditText timeText;
+    protected EditText eventText;
 
+    /**Event that is created*/
     protected Event createdEvent;
 
     private DatabaseReference mDatabase;
+
+    /*Debugging TAG. Use in log*/
+    private static final String TAG = Activity.class.getName();
+
+    protected String e_event;
+    protected String e_food;
+    protected String e_date;
+    protected String e_time;
+    protected String e_location;
 
     @Override
     public void onCreate(Bundle saveInstanceState) {
@@ -55,59 +70,57 @@ public class AddWindow extends Activity{
         this.huntButton = (Button) findViewById(R.id.hunt_button);
         this.cancelButton = (Button) findViewById(R.id.cancel_button);
         //text
-        this.foodText = (TextView) findViewById(R.id.food_edit_text);
-        this.eventText = (TextView) findViewById(R.id.event_edit_text);
-        this.dateText = (TextView) findViewById(R.id.date_edit_text);
-        this.timeText = (TextView) findViewById(R.id.time_edit_text);
-        this.locationText = (TextView) findViewById(R.id.location_edit_text);
+        this.foodText = (EditText)findViewById(R.id.food_edit_text);
+        this.eventText = (EditText)findViewById(R.id.event_edit_text);
+        this.dateText = (EditText)findViewById(R.id.date_edit_text);
+        this.timeText = (EditText)findViewById(R.id.time_edit_text);
+        this.locationText = (EditText)findViewById(R.id.location_edit_text);
     }
 
     /**
      * Init the button listener
-     * When the user click on Hunt: Validate the event. If the event is not correct display the error
-     *
+     * When the user click on Hunt: Validate the event.
+     * If the event is not correct display the error
      */
     public void initListener() {
-        //TODO: Fix the listener and te thread
+        //FIXME: Fix the listener and the thread
+
         huntButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Thread(new Runnable() {
-                    public void run() {
-                        if(!validateEvent()) {
-                            if (!validateTimeText()) {
-                                Toast.makeText(getApplicationContext(), "Missing time",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                            else if(!validateLocationText()) {
-                                Toast.makeText(getApplicationContext(), "Missing location",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                            else if(!validateDateText()) {
-                                Toast.makeText(getApplicationContext(), "Missing date or wrong format. " +
-                                                "Correct format: MM/dd/yyyy",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                            else if(!validateFoodText()) {
-                                Toast.makeText(getApplicationContext(), "Missing food",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        else {
-                            Toast.makeText(getApplicationContext(), "Add event",
-                                    Toast.LENGTH_SHORT).show();
-                            //something
-                            //sendEvent(createdEvent);
-                            //TODO: Use snack bar to display something
+                /** Get the value to the variable*/
+                AddWindow.this.e_date = AddWindow.this.dateText.getText().toString().trim();
+                AddWindow.this.e_event = AddWindow.this.eventText.getText().toString().trim();
+                AddWindow.this.e_food = AddWindow.this.foodText.getText().toString().trim();
+                AddWindow.this.e_time = AddWindow.this.timeText.getText().toString().trim();
+                AddWindow.this.e_location = AddWindow.this.locationText.getText().toString().trim();
 
-                            //finish();
-                        }
+                if(!validateEvent()) {
+                    if (!validateTimeText()) {
+                        Toast.makeText(getApplicationContext(), "Missing time",
+                                Toast.LENGTH_SHORT).show();
                     }
-                }).start();
+                    else if(!validateLocationText()) {
+                        Toast.makeText(getApplicationContext(), "Missing location",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    else if(!validateDateText()) {
+                        Toast.makeText(getApplicationContext(), "Missing date or wrong format. " +
+                                        "Correct format: MM/dd/yyyy",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    else if(!validateFoodText()) {
+                        Toast.makeText(getApplicationContext(), "Missing food",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    UploadTask uploadTask = new UploadTask();
+                    //event should be created at this point already
+                    uploadTask.execute(createdEvent);
+                }
             }
         });
-
-
 
         //just close the fragment
         cancelButton.setOnClickListener(new View.OnClickListener() {
@@ -118,52 +131,36 @@ public class AddWindow extends Activity{
         });
     }
 
-    public void sendEvent(Event event) {
-        //get the field from the list
-        //mDatabase.child("")
-        String key = this.mDatabase.child("events").push().getKey();
-        Map<String, Object> eventValues = createdEvent.toMap();
-
-        Map<String, Object> childUpdates = new HashMap<>();
-        childUpdates.put("/events/"+key, eventValues);
-
-
-        //send it to the server
-        mDatabase.updateChildren(childUpdates);
-    }
-
     public boolean validateEvent() {
         if (validateFoodText()
                 && validateDateText()
                 && validateLocationText()
                 && validateTimeText()) {
-            createdEvent = new Event(this.eventText.getText().toString(),
-                    this.locationText.getText().toString(),
-                    this.foodText.getText().toString(),
-                    this.dateText.getText().toString(),
-                    this.timeText.getText().toString());
+            createdEvent = new Event(this.e_event,
+                    this.e_location,
+                    this.e_food,
+                    this.e_date,
+                    this.e_time);
         }
         return false;
     }
 
     protected boolean validateFoodText() {
-        if (this.foodText.getText().toString().trim().equals("")) {
+        if (this.e_food.equals("")) {
             return false;
         }
         return true;
     }
 
     protected boolean validateLocationText() {
-        if(this.locationText.getText().toString().trim().equals("")) {
+        if(this.e_location.equals("")) {
             return false;
         }
         return true;
     }
 
     protected boolean validateDateText() {
-        //TODO: Check the format of the date input
-
-        if(this.dateText.getText().toString().trim().equals("")) {
+        if(this.e_date.equals("")) {
             return false;
         }
 
@@ -171,7 +168,7 @@ public class AddWindow extends Activity{
         dateFormat.setLenient(false);
         //Try the format
         try {
-            dateFormat.parse(this.dateText.getText().toString().trim());
+            dateFormat.parse(this.e_date.trim());
         }
         catch (ParseException e) {
             return false;
@@ -181,11 +178,63 @@ public class AddWindow extends Activity{
     }
 
     protected boolean validateTimeText() {
-        if(this.timeText.getText().toString().trim().equals("")) {
+        if(this.e_time.equals("")) {
             return false;
         }
 
         return true;
     }
 
+    /**
+     * This class handle uploading joke
+     */
+    public class UploadTask extends AsyncTask<Event, Void, String> {
+        /**
+         * Override this method to perform a computation on a background thread. The
+         * specified parameters are the parameters passed to {@link #execute}
+         * by the caller of this task.
+         * <p/>
+         * This method can call {@link #publishProgress} to publish updates
+         * on the UI thread.
+         *
+         * @param params The parameters of the task.
+         * @return A result, defined by the subclass of this task.
+         * @see #onPreExecute()
+         * @see #onPostExecute
+         * @see #publishProgress
+         */
+        @Override
+        protected String doInBackground(Event... params) {
+            //upload the event in here
+
+            try {
+                String key = AddWindow.this.mDatabase.child("events").push().getKey();
+                Map<String, Object> eventValues = createdEvent.toMap();
+
+                Map<String, Object> childUpdates = new HashMap<>();
+                childUpdates.put("/events/"+key, eventValues);
+
+
+                //send it to the server
+                mDatabase.updateChildren(childUpdates);
+            }
+            catch (Exception e) {
+                Log.d(AddWindow.this.TAG, "Error in downloading");
+            }
+            return "Success";
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(AddWindow.this, "Start uploading...", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPreExecute();
+            Toast.makeText(AddWindow.this, "Finish uploading...", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
